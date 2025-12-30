14920dfdfa3642bf6aba55ade7a1d460a1e7685e
Rudra <rud28b@gmail.com>
2025-12-30 16:42:15 +0530

    checkpoint

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,6 @@
-import { io } from "socket.io";
+import { io } from "socket.io-client";
 
-const VITE_SOCKET_URL = import.meta.env.SOCKET_URL;
+const VITE_SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 export const connectSocket = (token) =>
   io(VITE_SOCKET_URL, {
